cd244dd17ab252951fad50bcbdf0124b179baa01
Sarah Jimenez <57870970+sarahmarie23@users.noreply.github.com>
2022-03-05 13:44:00 -0800

    Update shift_cipher.py
    The shifting was off for letters near the end, so it works now

--- shift_cipher.py
+++ shift_cipher.py
@@ -1,19 +1,22 @@
 def shift_cipher(a_string, shift):
     # shift states how many letters each letter should be shifted
     # if shift = 3, then A = D, B = E
-   
+
+    #shift is 23
+    
     end_shift = 26 - shift 
-    range_begin = ord('Z') - shift + 1   
-    range_end = ord('Z') # z = 90
+    range_begin = ord('Z') - shift    
+    range_end = ord('Z') 
 
     converted_list = []
     a_string = a_string.upper()
         
     for char in a_string:
         if char.isalpha():
-            if ord(char) in range(range_begin, range_end+1):
-                converted_list.append(chr(ord(char) - end_shift+1))
-            else:
+            #letter is in latter part of alphabet, so subtract
+            if ord(char) in range(range_begin, range_end + 1):
+                converted_list.append(chr(ord(char) - end_shift))
+            else: #letter is in beginning, so add
                 converted_list.append(chr(ord(char) + shift))
         else:
             converted_list.append(char)
