e59e1128d795dfc2e868d83c8b0dc81e76a225c7
Sarah Jimenez <57870970+sarahmarie23@users.noreply.github.com>
2022-03-07 13:59:53 -0800

    Add files via upload
    For an affine cipher, a and b need to be coprime. This is an example of a substitution cipher.

---
+++ affine_cipher.py
@@ -0,0 +1,18 @@
+def affine_cipher(a_string, a, b):
+
+    encrypted_list = []
+    a_string = a_string.upper()
+
+    for char in a_string:
+        if char.isalpha():
+            c = (a * (ord(char)- 64) + b) % 26
+            encrypted_list.append(chr(c + 64))
+            
+        else:
+            encrypted_list.append(char)
+
+    
+    encrypted_string = "".join(encrypted_list)
+
+      
+    return encrypted_string
